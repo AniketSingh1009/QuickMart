71d4fb3b4c33a184aa91e862fa122a12b11db22f
Niket kumar <88975921+AniketSingh1009@users.noreply.github.com>
2025-04-29 18:09:21 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className='border-t'>
         <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
-            <p className='font-bold text-lg'> Developer :Team Sprinters</p>
+            <p className='font-bold text-lg'> All Rights Reserved @manit bhoal 2025 </p>
             <div className='flex items-center gap-4 justify-center text-2xl'>
                 <a href='' className='hover:text-yellow-200 transition-colors duration-300'>
                     <FaFacebook/>
